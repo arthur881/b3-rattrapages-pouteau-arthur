7280590ac0df3b9d62fab3ec0f5acb38abca57ca
Arthur Pouteau <arthur.pouteau92@gmail.com>
2024-08-04 18:24:56 +0200

    Add ProductsController

---
+++ 11-creationAPI/picardAPI/src/Controller/ProductsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductsController.php',
+        ]);
+    }
+}
